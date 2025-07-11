f56fc943f83d77f08e8c821a6c18565abe26b5e7
veryAngryKitten <kjobbagyi23@gmail.com>
2025-07-11 21:48:59 +0200

    Drew a fucking circle
    I drew a fucking circle in c++

--- main.cpp
+++ main.cpp
@@ -15,10 +15,40 @@ class body {
     Vec2 velocity;
     Vec2 acceleration;
 
-    void update(double dt);
-    void applyForce(const Vec2& force);
+    body(double mass, Vec2 position, Vec2 velocity, Vec2 acceleration) {
+        this->mass = mass;
+        this->position = position;
+        this->velocity = velocity;
+        this->acceleration = acceleration;
+    }
 
+    void update(double dt) {
+        velocity.x += acceleration.x * dt;
+        velocity.y += acceleration.y * dt;
+        position.x += velocity.x * dt;
+        position.y += velocity.y * dt;
+    }
+    void applyForce(const Vec2& force) {
+        acceleration.x = force.x / mass;
+        acceleration.y = force.y / mass;
+    }
+    
 };
+
+// draw 
+void drawCircle(float cx, float cy, float r, int num_segments) {
+
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(cx, cy); // center
+    for (int i = 0; i <= num_segments; i++) {
+        float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);
+        float x = r * cosf(theta);
+        float y = r * sinf(theta);
+        glVertex2f(cx + x, cy + y);
+    }
+    glEnd();
+    
+}
 
 int main() {
     GLFWwindow* window;
@@ -29,7 +59,7 @@ int main() {
     }
 
     //window creation
-    window = glfwCreateWindow(640, 480, "Hello Window", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "Body", NULL, NULL);
 
     if (!window) {
         glfwTerminate();
@@ -38,10 +68,25 @@ int main() {
 
     // make window context current
     glfwMakeContextCurrent(window);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, 800, 0, 600, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // body init
+    body earth(
+        5.972e24,
+        {0.0, 0.0}, // Center
+        {0.0, 0.0},
+        {0.0, 0.0}
+
+    );
 
     while (!glfwWindowShouldClose(window)) {
         // Render here
         glClear(GL_COLOR_BUFFER_BIT);
+        drawCircle(400.0f, 300.0f, 10.0f, 100);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
@@ -49,7 +94,6 @@ int main() {
         // Poll for and process events
         glfwPollEvents();
     }
-
 
     glfwTerminate();
     return 0;
